70a36ef555bd3a384ba8ab377b7bfe97e899364a
Jose Rodriguez <jose.rodriguez@zecovery.com>
2022-10-05 10:05:34 +0200

    test y phpstan cleanup

--- tests/EndpointsFormTest.php
+++ tests/EndpointsFormTest.php
@@ -3,6 +3,10 @@
 declare(strict_types=1);
 
 test('Hola mundo', function () {
+    expect(1)->toBe(1);
+});
+
+test('Request', function () {
     $this->getJson('laravel-library-boilerplate')
         ->assertOk()
         ->assertSee('Hola mundo');
